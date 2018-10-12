daea4468e1af046ed948567a26aa43c43c7baf18
no1harm <chen09869@gmail.com>
2018-10-12 13:59:41 +0800

    开始创建 PostList

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
     <Header></Header>
+    <Post-list></Post-list>
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
+import Header from "./components/Header";
+import PostList from './components/PostList'
 export default {
-  name: 'App',
-  components:{
-    Header
+  name: "App",
+  components: {
+    Header,
+    PostList
   }
-}
+};
 </script>
 
 <style>
-*{
-  margin: 0;padding: 0;
+* {
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      <div class="loading" v-if="isLoader">
+          <img src="../assets/loading.gif" >
+      </div>
+      <div class="listStyle">
+          <ul>
+              <li></li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PostList",
+  data() {
+    return {
+        isLoader:false
+    };
+  },
+  components: {}
+};
+</script>
+
+<style scoped>
+</style>
