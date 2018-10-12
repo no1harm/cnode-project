18c8535ef0b62e55c778e3b0d57a6bc4a7632207
no1harm <chen09869@gmail.com>
2018-10-12 14:26:03 +0800

    引入axios，获取API数据

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      <div class="loading" v-if="isLoader">
+      <div class="loading" v-if="isLoading">
           <img src="../assets/loading.gif" >
       </div>
       <div class="listStyle">
@@ -16,8 +16,28 @@ export default {
   name: "PostList",
   data() {
     return {
-        isLoader:false
+        isLoading:false
     };
+  },
+  methods:{
+      // 获取数据
+      getData(){
+          this.$axios.get('https://cnodejs.org/api/v1/topics',{
+              page:1,
+              limit:20
+          }).then((response)=>{
+              this.isLoading = false    // 加载成功后去除动画
+              console.log(response)
+          })
+          .catch((err)=>{
+              console.log(err)
+          })
+      }
+  },
+  // 在 Vue 实例挂载到 DOM 之前获取数据
+  beforeMount(){
+      this.isLoading = true
+      this.getData()
   },
   components: {}
 };
