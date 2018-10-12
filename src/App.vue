55fb4d92cd8ffaec11d74450ab8e18036a41ff6b
no1harm <chen09869@gmail.com>
2018-10-12 13:07:17 +0800

    创建 Header

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Header></Header>
   </div>
 </template>
 
 <script>
+import Header from './components/Header'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Header
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;padding: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <img src="../assets/cnodejs_light.svg" alt="">
+    <ul>
+      <li><a href="#">首页</a></li>
+      <li><a href="#">新手入门</a></li>
+      <li><a href="#">API</a></li>
+      <li><a href="#">关于</a></li>
+      <li><a href="#">注册</a></li>
+      <li><a href="#">登录</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Header',
+  data() {
+    return {
+
+    }
+  },
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+.header{
+  background-color: #444;
+  height:50px;
+}
+img{
+  max-width: 120px;
+  margin-left: 50px;
+  margin-top: 10px;
+}
+ul{
+  list-style: none;
+  float: right;
+  margin-right: 50px;
+}
+li{
+  display: inline-block;
+  line-height: 50px;
+  padding: 0 10px
+}
+a{
+  text-decoration: none;
+  color: #ccc;
+  font-size: 13px;
+  text-shadow: none;
+}
+</style>
